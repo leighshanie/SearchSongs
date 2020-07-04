956fbd308ebf7f68db33ba28489bceed7f82cdc2
Shanie Leigh <yao.h.wang@uni.sydney.edu.au>
2020-07-05 00:16:37 +0800

    init

---
+++ Shared/SearchSongsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SearchSongsApp.swift
+//  Shared
+//
+//  Created by Shane Leigh on 04/07/2020.
+//
+
+import SwiftUI
+
+@main
+struct SearchSongsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
